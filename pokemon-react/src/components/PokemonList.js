93c2da7d81be1168f053be1ad880b54f9625d5a1
YanethM <yaneth.mejiar@autonoma.edu.co>
2023-04-30 12:24:03 -0500

    Searcg pokemon using axios

---
+++ pokemon-react/src/components/PokemonList.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function PokemonList() {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [pokemonData, setPokemonData] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/')
+      .then(response => {
+        setPokemonList(response.data.results);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleSearchChange = event => {
+    setSearchQuery(event.target.value);
+  }
+
+  const handleSearchSubmit = event => {
+    event.preventDefault();
+
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${searchQuery}`)
+      .then(response => {
+        setPokemonData(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  return (
+    <div>
+      <h1>Pokemon List</h1>
+      <form onSubmit={handleSearchSubmit}>
+        <label htmlFor="searchQuery">Search for a Pokemon:</label>
+        <input
+          id="searchQuery"
+          type="text"
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {pokemonData && (
+        <div>
+          <h2>{pokemonData.name}</h2>
+          <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
+        </div>
+      )}
+      {!pokemonData && (
+        <ul>
+          {pokemonList.map(pokemon => (
+            <li key={pokemon.name}>{pokemon.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default PokemonList;
